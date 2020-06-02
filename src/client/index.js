58d640ec5e4f8371796d8cd6d911e1373cfe3039
Mona Assarandarban <mona.darban@hotmail.com>
2020-06-02 15:29:20 -0700

    express server fixed

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,17 @@
-import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-
 import './styles/resets.scss'
 
 console.log(checkForName);
 
 alert("I EXIST")
-alert("0-initial-setup test")
-console.log("CHANGE!!");
+
+export {
+    checkForName,
+    handleSubmit
+}
